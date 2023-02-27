7b74aa8dcdc37c68c5ad53a9ba3823f1f078f779
German Alexis <alexisdager93@hotmail.com>
2023-02-27 17:36:01 -0500

    implement of Args

--- src/helloworld/helloworld.resolver.ts
+++ src/helloworld/helloworld.resolver.ts
@@ -1,9 +1,30 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloworldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    description: 'Query Hola mundo return string',
+    name: 'hello',
+  })
   helloWorld(): string {
     return 'Hola mundo';
+  }
+
+  @Query(() => Float, {
+    description: 'Obtiene un numero ramdon decimal',
+    name: 'numberRamdonfloat',
+  })
+  getnumberRamdon(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    description: 'Obtiene un numero ramdon entero del 1 a TO (default 6)',
+    name: 'numberRamdonint',
+  })
+  ramdonFromZeroTo(
+    @Args('to', { nullable: true, type: () => Int }) to = 6,
+  ): number {
+    return Math.floor(Math.random() * to + 1);
   }
 }
